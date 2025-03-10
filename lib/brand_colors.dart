38f347986c6a6a615d2a25e4e045918440f00dc7
TaeHyunKim <kimharry200@gmail.com>
2025-03-10 18:59:07 -0400

    colors

--- lib/brand_colors.dart
+++ lib/brand_colors.dart
@@ -11,5 +11,15 @@ class BrandColors {
   static const Color colorPink = Color(0xFFE66C75);
   static const Color colorOragne = Color(0xFFe8913A);
   static const Color colorBlue = Color(0xFF2254A3);
-  static const Color colorAccentPurple = Color(0xFF45cd1);
+  static const Color colorAccentPurple = Color(0xFF4f5cd1);
+
+  static const Color colorText = Color(0xFF383635);
+  static const Color colorTextLight = Color(0xFF918D8D);
+  static const Color colorTextSemiLight = Color(0xFF737373);
+  static const Color colorTextDark = Color(0xFF292828);
+
+  static const Color colorGreen = Color(0xFF40cf89);
+  static const Color colorLightGray = Color(0xFFe2e2e2);
+  static const Color colorLightGrayFair = Color(0xFFe1e5e8);
+  static const Color colorDimText = Color(0xFFadadad);
 }
